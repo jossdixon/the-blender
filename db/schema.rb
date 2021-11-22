dd3eca6925a23476ce22076f63caa5977f2fcaf5
Joss Dixon <josscdixon@gmail.com>
2021-11-22 18:35:02 +0900

    profiles done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_092603) do
+ActiveRecord::Schema.define(version: 2021_11_22_093414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "village"
+    t.string "phone_number"
+    t.date "birthday"
+    t.date "join_date"
+    t.string "business_type"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_11_22_092603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
