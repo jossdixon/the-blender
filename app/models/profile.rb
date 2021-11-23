f48c3fb442335a306f9d0072450e87f69fffb522
Joss Dixon <josscdixon@gmail.com>
2021-11-23 13:18:02 +0900

    models, migrations, and validations added

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,10 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+
+  validates :village, presence: true
+  validates :phone_number, presence: true
+  validates :birthday, presence: true
+  validates :join_date, presence: true
+  validates :business_type, presence: true
+
 end
