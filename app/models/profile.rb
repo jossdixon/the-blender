b5ca3974c2efc8dcf963dc255614ec55efdbde5d
Michiharu Ono <michiharuono77@gmail.com>
2021-11-23 15:53:36 +0900

    still working on the search

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,17 +3,17 @@ class Profile < ApplicationRecord
 
   has_one_attached :photo
 
-  validates :village, presence: true
-  validates :phone_number, presence: true
-  validates :birthday, presence: true
-  validates :join_date, presence: true
-  validates :business_type, presence: true
+  # validates :village, presence: true
+  # validates :phone_number, presence: true
+  # validates :birthday, presence: true
+  # validates :join_date, presence: true
+  # validates :business_type, presence: true
 
   include PgSearch::Model
   pg_search_scope :global_search,
     against: [ :village, :business_type ],
     associated_against: {
-      director: [ :first_name, :last_name ]
+      user: [ :first_name, :last_name ]
     },
     using: {
       tsearch: { prefix: true }
