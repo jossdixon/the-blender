127334363a14191f8479b4c249f1d6a4f29d0370
Michiharu Ono <michiharuono77@gmail.com>
2021-11-23 16:28:06 +0900

    comment out deleted

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,11 +3,11 @@ class Profile < ApplicationRecord
 
   has_one_attached :photo
 
-  # validates :village, presence: true
-  # validates :phone_number, presence: true
-  # validates :birthday, presence: true
-  # validates :join_date, presence: true
-  # validates :business_type, presence: true
+  validates :village, presence: true
+  validates :phone_number, presence: true
+  validates :birthday, presence: true
+  validates :join_date, presence: true
+  validates :business_type, presence: true
 
   include PgSearch::Model
   pg_search_scope :global_search,
