00fcc9e4fafd3e3121a0c531e715cd7118718a48
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-23 15:39:35 +0900

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+User.destroy_all
+Profile.destroy_all
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row, header_converters: :symbol }
+filepath    = File.join(__dir__,'users.csv')
+
+CSV.foreach(filepath, csv_options) do |row|
+email = row[:email]
+encrypted_password = row[:encrypted_password]
+first_name = row[:first_name]
+last_name = row[:last_name]
+user = User.new(email: email, password: encrypted_password, first_name: first_name, last_name: last_name)
+user.save!
+village = row[:village]
+birthday = row[:birthday]
+phone_number = row[:phone_number]
+join_date = row[:join_date]
+business_type = row[:business_type]
+profile = Profile.new(
+  village: village,
+  birthday: birthday,
+  phone_number: phone_number,
+  join_date: join_date,
+  business_type: business_type,
+  user: user
+)
+profile.save!
+end
