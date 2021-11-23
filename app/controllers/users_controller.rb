2065a516bf95a945c119f684857d328234b1057a
Michiharu Ono <michiharuono77@gmail.com>
2021-11-23 14:32:07 +0900

    user controllers edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+    @user.profile.build
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :first_name, :last_name, profile_attributes: [:village, :phone_number, :birthday, :join_date, :business_type])
+  end
 end
