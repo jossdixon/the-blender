2d46e32dd2817494a819bcea6df5e31c5670eb0e
Joss Dixon <josscdixon@gmail.com>
2021-11-24 11:33:58 +0900

    table migrations and validations done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_051900) do
+ActiveRecord::Schema.define(version: 2021_11_24_023135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,15 +42,13 @@ ActiveRecord::Schema.define(version: 2021_11_23_051900) do
     t.float "total"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
     t.index ["loan_id"], name: "index_loanees_on_loan_id"
     t.index ["user_id"], name: "index_loanees_on_user_id"
   end
 
   create_table "loans", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.date "due_by"
-    t.float "instalments"
-    t.date "start_date"
     t.integer "status"
     t.integer "weeks"
     t.datetime "created_at", precision: 6, null: false
