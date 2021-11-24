3c62866e2607fbb5cfdcaf0da7ec63ee7b85fa0e
Michiharu Ono <michiharuono77@gmail.com>
2021-11-24 12:26:25 +0900

    nested form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # devise_for :users
   devise_for :users
   root to: 'pages#home'
   resources :profiles, only: [:index, :show, :new]
@@ -6,4 +7,6 @@ Rails.application.routes.draw do
     resources :loans, only: [:new, :create]
     resources :weekly_payments, only: [:new, :create]
   end
+  resources :users, only: [:new ]
+  post 'user_profile', to: "users#create", as: 'user_profile'
 end
