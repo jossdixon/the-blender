150acc288ca2226110f75ac52ec3955a49e93e21
Carlos Hernandez Topete <chtopete@gmail.com>
2021-11-26 16:04:12 +0900

    minor changes

--- app/javascript/packs/action_button.js
+++ app/javascript/packs/action_button.js
@@ -1,11 +1,11 @@
 function action_button () {
-  console.log(456456)
 
   const fab = document.querySelector('.fab');
-
+  if (fab) {
   fab.addEventListener('click', () => {
     fab.classList.toggle('active');
   })
+}
 }
 
 export {action_button}
