09aefc5e6675d6cf86b9b860650216c2dfe850ba
Joss Dixon <josscdixon@gmail.com>
2021-11-29 16:39:06 +0900

    autocomplete user input added to loans/new

--- app/javascript/components/slider.js
+++ app/javascript/components/slider.js
@@ -7,15 +7,11 @@ const rangeSlider = () => {
   console.log(value);
 
   slider.each(function () {
-    console.log('slide');
     value.each(function () {
       var value = $(this).prev().attr('value');
       $(this).html(value);
     });
-    console.log('slide again');
     range.on('input', function () {
-      console.log(this)
-      console.log(this.value)
       $(this).parent().next(value).html(this.value);
     });
   });
